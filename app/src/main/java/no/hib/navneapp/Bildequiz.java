b45af0ce3186badc8f5023188d36b0dd68a0e8c8
bromle666 <endrenordang@gmail.com>
2015-03-25 23:20:29 +0100

    must implement quiz button on welcome screen

--- app/src/main/java/no/hib/navneapp/Bildequiz.java
+++ app/src/main/java/no/hib/navneapp/Bildequiz.java
@@ -7,10 +7,26 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.List;
 
 
 public class Bildequiz extends Fragment {
+
+    private int score;
+    private int counter;
+    private List<Person> personliste;
+    private List<String> navneListe;
+    private Person currentPerson;
+    private ImageView imageView;
+    private Spinner spinner;
+    private Button button;
 
     public Bildequiz() {
         // Required empty public constructor
@@ -20,13 +36,56 @@ public class Bildequiz extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bildequiz, container, false);
+
+        final View returnView = inflater.inflate(R.layout.fragment_bildequiz, container, false);
+
+        score = 0;
+        counter = 0;
+        personliste = ((MainActivity)getActivity()).getPersonListe();
+        navneListe = ((MainActivity)getActivity()).getNames();
+        imageView = (ImageView) returnView.findViewById(R.id.quizImageView);
+        spinner = (Spinner) returnView.findViewById(R.id.quizSpinner);
+        button = (Button) returnView.findViewById(R.id.quizButton);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(),
+                android.R.layout.simple_list_item_1, navneListe);
+        spinner.setAdapter(adapter);
+
+        currentPerson = personliste.get(counter);
+        imageView.setImageDrawable(currentPerson.getBilde());
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (spinner.getSelectedItem().equals(currentPerson.getNavn())) {
+                    Toast.makeText(getActivity(), "Riktig",Toast.LENGTH_SHORT).show();
+                    score++;
+                }
+                else {
+                    Toast.makeText(getActivity(), "Galt",Toast.LENGTH_SHORT).show();
+                }
+                counter++;
+
+
+                if (personliste.get(counter) != null) {
+                    currentPerson = personliste.get(counter);
+                    imageView.setImageDrawable(currentPerson.getBilde());
+                }
+                else {
+                    Toast.makeText(getActivity(), "Din score ble " + score ,Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+        return returnView;
     }
 
 
